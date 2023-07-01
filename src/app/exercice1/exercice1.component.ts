82451d27fb0a0d59e057294f45beffc95660dfda
Marine <marineg.dev@gmail.com>
2023-07-02 00:00:36 +0200

    Implement data binding in exercice1

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-exercice1',
   templateUrl: './exercice1.component.html',
-  styleUrls: ['./exercice1.component.css']
+  styleUrls: ['./exercice1.component.css'],
 })
 export class Exercice1Component {
+  public words: [string, string][] = [
+    ['génial', 'color: red; fontWeight: bold;'],
+    ['super', 'color: orange; fontStyle: italic;'],
+    ['extraordinaire', 'color: teal; textTransform: uppercase;'],
+    ['passionnant', 'padding: 4px; backgroundColor: lavender;'],
+    ['fascinant', 'color: black; backgroundColor: lightgrey; padding: 2px'],
+  ];
 
+  public currentIndex = 0;
+
+  public changeWord(): void {
+    const newIndex = this.currentIndex + 1;
+    this.currentIndex = newIndex === this.words.length ? 0 : newIndex;
+  }
 }
