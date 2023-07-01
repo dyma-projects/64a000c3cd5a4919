e90ca52be9c8e5bb7bf2b505be51eb754a4cffc1
Marine <marineg.dev@gmail.com>
2023-07-02 00:06:01 +0200

    Add new component exercice3 to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Exercice1Component } from './exercice1/exercice1.component';
 import { Exercice2Component } from './exercice2/exercice2.component';
+import { Exercice3Component } from './exercice3/exercice3.component';
 
 @NgModule({
-  declarations: [AppComponent, Exercice1Component, Exercice2Component],
+  declarations: [AppComponent, Exercice1Component, Exercice2Component, Exercice3Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
